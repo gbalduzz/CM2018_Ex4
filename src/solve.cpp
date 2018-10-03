968ed7bbcefbf2f973d274d371fad10773dff498
gbalduzz <gbalduzz@itp.phys.ethz.ch>
2018-10-03 21:33:26 +0200

    use json input.

--- src/solve.cpp
+++ src/solve.cpp
@@ -2,27 +2,35 @@
 #include <fstream>
 #include <iostream>
 
+#include "json11.hpp"
 #include "second_order_solver.hpp"
 
 int main() {
-  const double x0 = 3. / 4. * M_PI;
-  const double v0 = 0;
+  auto parameters = json11::Json::parseFile(SOURCE_DIR "/input.json");
+
   const double g_over_l = 1.;
   const int n_periods = 3;
 
-  SecondOrderSolver solver(x0, v0, g_over_l, 1e-6);
+  const double x0 = parameters["x0"].number_value() * M_PI;
+  const double v0 = parameters["v0"].number_value();
+  const double dt = parameters["dt"].number_value();
+
+  SecondOrderSolver solver(x0, v0, g_over_l, dt);
 
   std::ofstream out("second_order_ODE_solution.txt");
   out << "# time \t theta \t theta_dot\n";
   solver.printConfiguration(out);
 
+  const int steps_per_sample = parameters["steps per sample"].int_value();
+
   while (solver.periods() < n_periods) {
-    for (int i = 0; i < 100; ++i)
+    for (int i = 0; i < steps_per_sample; ++i)
       solver.doStep();
     solver.printConfiguration(out);
   }
   out.close();
 
   std::cout << "Average period: " << solver.get_average_period()
-            << "\nAmplitude shift: " << solver.get_amplitude_shift() << std::endl;
+            << "\nAmplitude shift: " << solver.get_amplitude_shift()
+            << std::endl;
 }
